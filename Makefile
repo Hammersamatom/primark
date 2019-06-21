e27cd3c2ef8f06ba848ed1078fe8702037ca43d1
Hammersamatom <hammersamatom@gmail.com>
2019-06-20 20:05:06 -0400

    Multithreading works, I guess?

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 CC=g++
 CPPFLAGS=-march=native -mtune=native -O3 -fno-plt -flto -fno-exceptions
+LDFLAGS=-lpthread
+DEBUG =-Wall -Wextra -Werror -pedantic
 PROGNAME=primark
 
 $(PROGNAME): main.o
-	$(CC) -o $(PROGNAME) main.o $(CPPFLAGS)
+	$(CC) -o $(PROGNAME) main.o $(CPPFLAGS) $(DEBUG) $(LDFLAGS)
 
 strip:
 	strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag $(PROGNAME)
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
-#include <ctime> 
+#include <ctime>
+#include <chrono>
 #include <vector>
 #include <stdio.h>
 #include <math.h>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+
+
+std::vector<std::thread> threads;
+std::vector<double> globalPrimes;
+std::mutex mylock;
 
 void primes()
 {
     std::vector<unsigned long> localPrimes;
-    clock_t startTicks = clock();
+    auto startTime = std::chrono::high_resolution_clock::now();
+    auto diffTime = 0;
     unsigned long n = 0;
 
-    while ((float)startTicks/CLOCKS_PER_SEC < 60)
+    while (diffTime < 6000)
     {
         bool flag = true;
 
@@ -30,14 +40,41 @@ void primes()
         n++;
 
         // Reset clock to stop while loop
-        startTicks = clock() - startTicks;
+        diffTime = std::chrono::duration<double, std::centi>(std::chrono::high_resolution_clock::now() - startTime).count();
     }
 
-    printf("Final score: %.2lf\n", localPrimes.size()/(((float)startTicks/CLOCKS_PER_SEC)*100));
+    //printf("Final score: %.2lf\n", localPrimes.size()/(((float)startTicks/CLOCKS_PER_SEC)*100));
+
+    mylock.lock();
+
+    globalPrimes.push_back((double)localPrimes.size()/6000);
+
+    mylock.unlock();
+
+}
+
+void join_all(std::vector<std::thread>& threadVect)
+{
+    // I thought this would be simpler than the example I found. Condensed into one function.
+    for (int i = 0; i < threadVect.size(); i++)
+    {
+        threadVect[i].join();
+    }
 }
 
 int main()
 {
-    primes();
+    for (int i = 0; i < std::thread::hardware_concurrency(); i++)
+    {
+        threads.push_back(std::thread(primes));
+    }
+    
+    join_all(threads);
+
+    std::sort(globalPrimes.begin(),globalPrimes.end());
+
+    for (int i = 0; i < globalPrimes.size(); i++)
+        printf("%lf\n", globalPrimes[i]);
+
     return 0;
 }
