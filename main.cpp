db304d325e132c9fb2ed4b776d242aadb477772a
Hammersamatom <hammersamatom@gmail.com>
2019-06-26 16:11:29 -0400

    Switched to ASYNC for testing purposes. Showed lower amount of context switches, but this could be an error.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
-#include <ctime>
 #include <chrono>
 #include <vector>
 #include <stdio.h>
 #include <math.h>
 #include <thread>
 #include <mutex>
-#include <algorithm>
+#include <future>
 
 unsigned long threadCount = 0;
 
-std::mutex mylock;
-std::vector<std::thread> threads;
-std::vector<double> multiPrimes;
+//std::mutex mylock;
+//std::vector<std::thread> threads;
+std::vector<std::future<double>> returns;
+//std::vector<double> multiPrimes;
 
-void primes()
+double primes()
 {
     std::vector<unsigned long> localPrimes;
     auto startTime = std::chrono::high_resolution_clock::now();
     double diffTime = 0;
     unsigned long n = 0;
-    //unsigned long correct = 0;
 
     while (diffTime < 6000)
     {
@@ -37,7 +36,6 @@ void primes()
         if (flag)
         {
             localPrimes.push_back(n);
-            //correct++;
         }
 
         n++;
@@ -46,13 +44,11 @@ void primes()
         diffTime = std::chrono::duration<double, std::centi>(std::chrono::high_resolution_clock::now() - startTime).count();
     }
 
-    mylock.lock();
+    //mylock.lock();
+    //multiPrimes.push_back((double)localPrimes.size()/diffTime);
+    //mylock.unlock();
 
-    multiPrimes.push_back((double)localPrimes.size()/diffTime);
-    //multiPrimes.push_back(correct/diffTime);
-
-    mylock.unlock();
-
+    return (double)localPrimes.size()/diffTime;
 }
 
 void join_all(std::vector<std::thread>& threadVect)
@@ -78,15 +74,21 @@ int main(int argc, char* argv[])
 
     for (int i = 0; i < threadCount; i++)
     {
-        threads.push_back(std::thread(primes));
+	returns.push_back(std::async(primes));
+        //threads.push_back(std::thread(primes));
     }
-    join_all(threads);
+    //join_all(threads);
 
     double actualTotal = 0;
 
-    for (int i = 0; i < multiPrimes.size(); i++)
+    //for (int i = 0; i < multiPrimes.size(); i++)
+    //{
+    //    actualTotal += multiPrimes[i]; 
+    //}
+
+    for (int i = 0; i < returns.size(); i++)
     {
-        actualTotal += multiPrimes[i]; 
+	actualTotal += returns[i].get();
     }
 
     printf("Average Multi-Core Score: %lf\n", actualTotal/threadCount);
